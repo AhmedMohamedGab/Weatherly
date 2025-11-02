16e03ccccfbf261c46ca2631938bb6f4c81ee774
Ahmed Gaballah <ahmedgaballah055@gmail.com>
2025-11-02 06:58:02 +0200

    Implemented and tested fetchWeatherData() asyncrounus function to get weather using visualcrossing weather API.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 import { fetchTheme, changeTheme, showHideXBtn, clearInput, showToast } from "./ui.js";
 
+const APIKey = 'HZ6DR5BEU5FT5YH8JR5CBA4QL'; // visualcrossing API key
+
 const themeBtn = document.getElementById('theme-btn');  // theme button
 const searchInput = document.getElementById('search-input');  // search box
 const xBtn = document.getElementById('x-btn');  // x button
@@ -22,31 +24,43 @@ xBtn.addEventListener('click', clearInput);
 
 // on clicking on location button
 locationBtn.addEventListener('click', () => {
-    if ("geolocation" in navigator) {   // check if geolocation service is supported in the browser
-        showToast('map-pin', 'Getting your location.');
-        getLocation();
-    } else {    // if geolocation service is not supported in the browser (in old browsers)
-        alert("Geolocation is not supported in this browser.");
+    if ("geolocation" in navigator) {   // check if geolocation service is supported by the browser
+        showToast('map-pin', 'Getting your location...');
+        fetchWeatherData();
+    } else {    // if geolocation service is not supported by the browser (in old browsers)
+        alert("Geolocation is not supported by this browser.");
     }
 });
 
-async function getLocation() {
+// fetch weather data using visualcrossing API
+async function fetchWeatherData() {
     try {
+        const coordinates = await getLocation();    // Wait for getLocation Promise to resolve
+        const [lat, lon] = coordinates;
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}?key=${APIKey}&unitGroup=metric&elements=datetime,tempmax,tempmin,temp,feelslike,humidity,precipprob,windspeed,pressure,visibility,conditions,icon`);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        showToast('x', 'Could not get weather data.');
+    }
+}
+
+// get user's location
+function getLocation() {
+    return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 const { latitude, longitude } = position.coords;
-                showToast('check', `Your location is ( ${latitude}, ${longitude} ) !`);
+                resolve([latitude, longitude]);
             },
             (err) => {
-                showToast('x', 'Could not get your location. Please enable location access and try again.');
+                reject(showToast('x', 'Could not get your location. Please enable location access and try again.'));
             },
             {
                 enableHighAccuracy: false, // start low accuracy for faster results
-                timeout: 10000,            // wait up to 10s
+                timeout: 15000,            // wait up to 15s
                 maximumAge: 0
             }
         );
-    } catch (error) {
-        alert("Could not get your location. Please enable location access and try again.");
-    }
+    });
 }
